f4e881c496c9fb23995e269cfe9533628d2489ab
Documenter.jl <documenter@juliadocs.github.io>
2022-03-21 01:15:37 +0000

    build based on 6a5bb34

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FDMT","category":"page"},{"location":"#FDMT","page":"Home","title":"FDMT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FDMT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FDMT]","category":"page"}]
+}
