28e0998a2b3765a35011c2e3125ba3f8941b6d07
Kiran Shila <me@kiranshila.com>
2022-04-11 16:20:08 -0700

    gotta go fast!

--- src/FDMT.jl
+++ src/FDMT.jl
@@ -1,5 +1,7 @@
 module FDMT
 
+using LoopVectorization
+
 const KDM = 4.148808e3 # MHz^2 s pc^-1 cm^3
 
 struct InputBlock{T1} <: AbstractMatrix{T1}
@@ -11,6 +13,10 @@ struct InputBlock{T1} <: AbstractMatrix{T1}
     t_samp::Real
     # Dimension time is in
     t_dim::Integer
+    # Dimension freq is in
+    f_dim::Integer
+    # Dispersion delay across the bands covered by the input block
+    Δkdisp::Real
 end
 
 struct OutputBlock{T1} <: AbstractMatrix{T1}
@@ -21,6 +27,8 @@ struct OutputBlock{T1} <: AbstractMatrix{T1}
     y_max::Real
     # Dimension time is in
     t_dim::Integer
+    # Dimension DM is in
+    dm_dim::Integer
 end
 
 # Methods for custom array types
@@ -30,45 +38,27 @@ Base.getindex(ib::InputBlock, i::Int, j::Int) = ib.data[i, j]
 Base.getindex(ob::OutputBlock, i::Int, j::Int) = ob.data[i, j]
 
 # Default constructors
-function InputBlock(data, f_ch1, f_chn, t_samp)
+function InputBlock(data, f_ch1, f_chn, t_samp, t_dim)
     @assert f_ch1 >= f_chn "Channel 1 must be higher than Channel N"
-    return InputBlock(data, f_ch1, f_chn, t_samp, 1)
+    Δkdisp = KDM * (f_chn^-2 - f_ch1^-2)
+    f_dim = t_dim == 1 ? 2 : 1
+    return InputBlock(data, f_ch1, f_chn, t_samp, t_dim, f_dim, Δkdisp)
 end
 
 function OutputBlock(ib::InputBlock{T}, y_min, y_max) where {T}
     n_trials = y_max - y_min + 1
     # We want to keep time in the same axis here
-    data = ib.t_dim == 1 ? zeros(T, n_samp(ib), n_trials) : zeros(T, n_trials, n_samp(ib))
-    return OutputBlock(data, y_min, y_max, ib.t_dim)
+    n_samp = size(ib)[ib.t_dim]
+    data = ib.t_dim == 1 ? zeros(T, n_samp, n_trials) : zeros(T, n_trials, n_samp)
+    dm_dim = ob.t_dim == 1 ? 2 : 1
+    return OutputBlock(data, y_min, y_max, ib.t_dim, ib.f_dim)
 end
 
-# The dimension that isn't time
-f_dim(ib::InputBlock) = ib.t_dim == 1 ? 2 : 1
-
-# The dimension that isn't time
-dm_dim(ob::OutputBlock) = ob.t_dim == 1 ? 2 : 1
-
-# Number of frequency channels
-n_ch(ib::InputBlock) = size(ib)[f_dim(ib)]
-
-# Number of time samples
-n_samp(ib::InputBlock) = size(ib)[ib.t_dim]
-n_samp(ob::OutputBlock) = size(ob)[ob.t_dim]
-
-# Number of DM trials
-n_trials(ob::OutputBlock) = size(ob)[dm_dim(ob)]
-
-# Frequency range
-freqs(ib::InputBlock) = range(; start=ib.f_ch1, stop=ib.f_chn, length=n_ch(ib))
-
 # Channel spacing
-f_step(ib::InputBlock) = (ib.f_ch1 - ib.f_chn) / n_ch(ib)
-
-# Dispersion delay across the bands covered by the input block
-Δkdisp(ib::InputBlock) = KDM * (ib.f_chn^-2 - ib.f_ch1^-2)
+f_step(ib::InputBlock) = (ib.f_ch1 - ib.f_chn) / size(ib)[ib.f_dim]
 
 # Natural DM step of FDMT for this block
-dm_step(ib::InputBlock) = ib.t_samp / Δkdisp(ib)
+dm_step(ib::InputBlock) = ib.t_samp / ib.Δkdisp
 
 function split(ib::InputBlock)
     # The split occurs at the channel where half the dispersion
@@ -77,11 +67,12 @@ function split(ib::InputBlock)
     i = round(Int, (ib.f_ch1 - f) / f_step(ib))
 
     # New frequency ranges
-    head_end = freqs(ib)[i]
-    tail_start = freqs(ib)[i + 1]
+    step = f_step(ib)
+    head_end = ib.f_ch1 - i * step
+    tail_start = ib.f_ch1 - (i + 1) * step
 
     # New data slices
-    ch = n_ch(ib)
+    ch = size(ib)[ib.f_dim]
     head_slice = ib.t_dim == 1 ? view(ib.data, :, 1:i) : view(ib.data, 1:i, :)
     tail_slice = ib.t_dim == 1 ? view(ib.data, :, (i + 1):ch) : view(ib.data, (i + 1):ch, :)
 
@@ -92,45 +83,48 @@ function split(ib::InputBlock)
     return head, tail
 end
 
+circmod(x, y) = mod(x - 1, y) + 1
+
 function transform_recursive(block, y_min, y_max)
     out = OutputBlock(block, y_min, y_max)
     # Base Case
-    if n_ch(block) == 1
+    if size(block)[block.f_dim] == 1
         if block.t_dim == 1
-            out.data[:,1] = block.data[:,1]
+            out.data[:, 1] = block.data[:, 1]
         else
-            out.data[1,:] = block.data[1,:]
+            out.data[1, :] = block.data[1, :]
         end
         return out
     end
     # Split
     head, tail = split(block)
     # Transform
-    y_min_head = trunc(Int, y_min * Δkdisp(head) / Δkdisp(block) + 0.5)
-    y_max_head = trunc(Int, y_max * Δkdisp(head) / Δkdisp(block) + 0.5)
+    y_min_head = trunc(Int, y_min * head.Δkdisp / block.Δkdisp + 0.5)
+    y_max_head = trunc(Int, y_max * head.Δkdisp / block.Δkdisp + 0.5)
     transformed_head = transform_recursive(head, y_min_head, y_max_head)
 
-    y_min_tail = trunc(Int, y_min * Δkdisp(tail) / Δkdisp(block) + 0.5)
-    y_max_tail = trunc(Int, y_max * Δkdisp(tail) / Δkdisp(block) + 0.5)
+    y_min_tail = trunc(Int, y_min * tail.Δkdisp / block.Δkdisp + 0.5)
+    y_max_tail = trunc(Int, y_max * tail.Δkdisp / block.Δkdisp + 0.5)
     transformed_tail = transform_recursive(tail, y_min_tail, y_max_tail)
 
-    _n_samp = n_samp(out)
+    n_samp = size(out)[out.t_dim]
 
     # Merge
     @turbo for y in y_min:y_max
         # yh = delay across head band
-        yh = floor(Int, y * Δkdisp(head) / Δkdisp(block) + 0.5)
+        yh = floor(Int, y * head.Δkdisp / block.Δkdisp + 0.5)
         # yt = delay across tail band
-        yt = floor(Int, y * Δkdisp(tail) / Δkdisp(block) + 0.5)
+        yt = floor(Int, y * tail.Δkdisp / block.Δkdisp + 0.5)
         # yb = delay at interface between head and tail
         yb = y - yh - yt
         ih = yh - transformed_head.y_min + 1
         it = yt - transformed_tail.y_min + 1
         i = y - out.y_min + 1
         # Update FIXME
-        for j in 1:_n_samp
-            j_shift = mod1(j + yh - yb, _n_samp)
-            out.data[j, i] = transformed_head[j, ih] + transformed_tail[j_shift, it]
+        for j in 1:n_samp
+            j_shift = circmod(j + yh - yb, n_samp)
+            out.data[j, i] = transformed_head.data[j, ih] +
+                             transformed_tail.data[j_shift, it]
         end
     end
 
