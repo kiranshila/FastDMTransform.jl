2c77ab7176f99a95b0030ac983b4ece8083db922
Kiran Shila <me@kiranshila.com>
2022-04-11 18:19:16 -0700

    Really type stable

--- src/FDMT.jl
+++ src/FDMT.jl
@@ -4,31 +4,36 @@ using LoopVectorization
 
 const KDM = 4.148808e3 # MHz^2 s pc^-1 cm^3
 
-struct InputBlock{T1} <: AbstractMatrix{T1}
-    data::AbstractMatrix{T1}
+struct InputBlock{T<:Real,
+                  M<:AbstractMatrix{T},
+                  TF<:Real,
+                  TT<:Real,
+                  TKD<:Real} <: AbstractMatrix{T}
+    data::M
     # Frequency of channel 1 and N
-    f_ch1::Real
-    f_chn::Real
+    f_ch1::TF
+    f_chn::TF
     # Time step in seconds
-    t_samp::Real
+    t_samp::TT
     # Dimension time is in
-    t_dim::Integer
+    t_dim::Int
     # Dimension freq is in
-    f_dim::Integer
+    f_dim::Int
     # Dispersion delay across the bands covered by the input block
-    Δkdisp::Real
+    Δkdisp::TKD
 end
 
-struct OutputBlock{T1} <: AbstractMatrix{T1}
-    data::AbstractMatrix{T1}
+struct OutputBlock{T<:Real,
+                   M<:AbstractMatrix{T}} <: AbstractMatrix{T}
+    data::M
     # Dispersion delay (s) for first DM trial
-    y_min::Real
+    y_min::Int
     # Dispersion delay (s) for last DM trial
-    y_max::Real
+    y_max::Int
     # Dimension time is in
-    t_dim::Integer
+    t_dim::Int
     # Dimension DM is in
-    dm_dim::Integer
+    dm_dim::Int
 end
 
 # Methods for custom array types
@@ -50,12 +55,11 @@ function OutputBlock(ib::InputBlock{T}, y_min, y_max) where {T}
     # We want to keep time in the same axis here
     n_samp = size(ib)[ib.t_dim]
     data = ib.t_dim == 1 ? zeros(T, n_samp, n_trials) : zeros(T, n_trials, n_samp)
-    dm_dim = ob.t_dim == 1 ? 2 : 1
     return OutputBlock(data, y_min, y_max, ib.t_dim, ib.f_dim)
 end
 
 # Channel spacing
-f_step(ib::InputBlock) = (ib.f_ch1 - ib.f_chn) / size(ib)[ib.f_dim]
+f_step(ib::InputBlock) = (ib.f_ch1 - ib.f_chn) / size(ib.data)[ib.f_dim]
 
 # Natural DM step of FDMT for this block
 dm_step(ib::InputBlock) = ib.t_samp / ib.Δkdisp
@@ -68,24 +72,24 @@ function split(ib::InputBlock)
 
     # New frequency ranges
     step = f_step(ib)
-    head_end = ib.f_ch1 - i * step
-    tail_start = ib.f_ch1 - (i + 1) * step
+    head_start, head_end = promote(ib.f_ch1, ib.f_ch1 - i * step)
+    tail_start, tail_end = promote(ib.f_ch1 - (i + 1) * step, ib.f_chn)
 
     # New data slices
     ch = size(ib)[ib.f_dim]
-    head_slice = ib.t_dim == 1 ? view(ib.data, :, 1:i) : view(ib.data, 1:i, :)
-    tail_slice = ib.t_dim == 1 ? view(ib.data, :, (i + 1):ch) : view(ib.data, (i + 1):ch, :)
+    head_slice = view(ib.data, :, 1:i)
+    tail_slice = view(ib.data, :, (i + 1):ch)
 
     # New blocks
-    head = InputBlock(head_slice, ib.f_ch1, head_end, ib.t_samp, ib.t_dim)
-    tail = InputBlock(tail_slice, tail_start, ib.f_chn, ib.t_samp, ib.t_dim)
+    head = InputBlock(head_slice, head_start, head_end, ib.t_samp, ib.t_dim)
+    tail = InputBlock(tail_slice, tail_start, tail_end, ib.t_samp, ib.t_dim)
 
     return head, tail
 end
 
-circmod(x, y) = mod(x - 1, y) + 1
+@inline circmod(x, y) = mod(x - 1, y) + 1
 
-function transform_recursive(block, y_min, y_max)
+function transform_recursive(block::InputBlock, y_min::Int, y_max::Int)
     out = OutputBlock(block, y_min, y_max)
     # Base Case
     if size(block)[block.f_dim] == 1
@@ -107,7 +111,8 @@ function transform_recursive(block, y_min, y_max)
     y_max_tail = trunc(Int, y_max * tail.Δkdisp / block.Δkdisp + 0.5)
     transformed_tail = transform_recursive(tail, y_min_tail, y_max_tail)
 
-    n_samp = size(out)[out.t_dim]
+    n_samp = size(out.data)[out.t_dim]
+    j_range = 1:n_samp
 
     # Merge
     @turbo for y in y_min:y_max
@@ -121,7 +126,7 @@ function transform_recursive(block, y_min, y_max)
         it = yt - transformed_tail.y_min + 1
         i = y - out.y_min + 1
         # Update FIXME
-        for j in 1:n_samp
+        for j in j_range
             j_shift = circmod(j + yh - yb, n_samp)
             out.data[j, i] = transformed_head.data[j, ih] +
                              transformed_tail.data[j_shift, it]
@@ -131,16 +136,18 @@ function transform_recursive(block, y_min, y_max)
     return out
 end
 
-function transform(data, f_ch1, f_chn, t_samp, dm_min, dm_max, t_dim)
+function transform(data::AbstractMatrix, f_ch1::Real, f_chn::Real, t_samp::Real,
+                   dm_min::Real, dm_max::Real, t_dim::Int)
     @assert dm_min >= 0 "Minimum DM must be zero"
     @assert dm_max >= dm_min "Maximum DM must be greater than the minimum"
 
     # Build input block
     block = InputBlock(data, f_ch1, f_chn, t_samp, t_dim)
+    dm_step = t_samp / block.Δkdisp
 
     # Convert DMs to delays in sample space
-    y_min = trunc(Int, dm_min / dm_step(block))
-    y_max = ceil(Int, dm_max / dm_step(block))
+    y_min = trunc(Int, dm_min / dm_step)
+    y_max = ceil(Int, dm_max / dm_step)
 
     # Perform transformation
     return transform_recursive(block, y_min, y_max)
