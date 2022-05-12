f513a3b7cddc1c6e01c57f39c53bed9616f4b48c
Kiran Shila <me@kiranshila.com>
2022-05-12 12:02:52 -0700

    Better test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,14 @@
-using FastDMTransform, NPZ, Test
+using FastDMTransform, NPZ, Test, Statistics
+
+function standardize(A; dims=1)
+    μ = mean(A; dims=dims)
+    σ = std(A; mean=μ, dims=dims)
+    return @. (A - μ) / σ
+end
 
 @testset "FastDMTransform.jl" begin
     pulse = npzread("../pulse.npz")
-    dedispersed = npzread("../dedispersed.npz")
-    dedisped = fdmt(pulse,1500,1200,1e-3,0,2000)
-    @test isapprox(dedisped,dedispersed; atol=0.01)
+    dedispersed = standardize(npzread("../dedispersed.npz"))
+    dedisped = standardize(fdmt(pulse, 1500, 1200, 1e-3, 0, 2000))
+    @test findmax(dedisped) == findmax(dedispersed)
 end
